045f57f199e82edd74e08cf5bbd842efb2050e06
venkkatiosameripro <vguggilapu@ameripro-solutions.com>
2019-09-30 22:45:20 +0530

    changes

--- ios-swift/CountlyTestApp-iOS-swift/TestViewControllerModal.swift
+++ ios-swift/CountlyTestApp-iOS-swift/TestViewControllerModal.swift
@@ -14,6 +14,9 @@ class TestViewControllerModal: UIViewController
     }
 
 
+    
+    
+    
     @IBAction func onClick_dismiss(_ sender: Any)
     {
         self.dismiss(animated:true, completion: (() -> Void)?{})
